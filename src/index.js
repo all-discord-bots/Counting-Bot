3b3e0457f57048162269086e04fd76dac4129380
BannerBomb <BannerBomb55@gmail.com>
2017-11-25 00:03:17 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const Eris = require("eris");
+const config = require("../config/config.json");
+
+var bot = new Eris(config.token);
+
+var counter = 0;
+var lastCountUser = 0;
+
+bot.on("ready", () => {
+    console.log("Ready to moderate counting!");
+    console.log("Logged in as " + bot.user.username + "#" + bot.user.discriminator + ".");
+
+    bot.editStatus("online", { name: "counting~!", type: 0 });
+});
+
+bot.on("messageCreate", (msg) => {
+
+    // ping command ---
+    if (msg.content === ((config.prefix) + "ping")) {
+        bot.createMessage(msg.channel.id, 'Pinging...').then(sent => {
+			    sent.edit(`**Pong!** \nMy latency is \`${sent.timestamp - msg.timestamp}ms\`.`);
+		    })
+    };
+
+    // main module ---
+    if(msg.channel.id != (config.channel)) {
+        return;
+
+    } else {
+      var newCount = (counter + 1);
+
+      if (msg.content != newCount) {
+        msg.delete("Auto-removed wrong or non-related message in counting channel.");
+
+      } else if (msg.author.id === lastCountUser) {
+        msg.delete("Auto-removed user's double message in counting channel.");
+
+      } else {
+        counter = newCount;
+        lastCountUser = msg.author.id;
+      }
+    };
+
+});
+
+bot.connect();
