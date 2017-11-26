560cb7c0c136819dfbe65e98f3dea8ffb02cfd38
BannerBomb <BannerBomb55@gmail.com>
2017-11-25 19:36:10 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,98 @@
 const Eris = require("eris");
+const fs = require("fs");
+const config = require("../config/config.json");
+var bot = new Eris(config.token);
+
+//let messups = JSON.parse(fs.readFileSync("./messups.json", "utf8"));
+let count = JSON.parse(fs.readFileSync("./count.json", "utf8"));
+
+var counter = count["count"];
+
+bot.on("ready", () => {
+	console.log("Ready to moderate counting!");
+	console.log("Logged in as " + bot.user.username + "#" + bot.user.discriminator + ".");
+	
+	bot.editStatus("online", { name: "1 2 3...", type: 0 });
+});
+
+bot.on("messageCreate", (msg) => {
+	//let cantcount = msg.guild.roles.get("384107437998735363");
+	if (msg.author.bot) return;
+	if (msg.channel.id == (config.channel)) {
+		if (msg.content == counter) {
+			counter++;
+			fs.writeFile("./count.json", JSON.stringify(count), (err) => {
+			if (err) {
+				console.error(err);
+			} else {
+				count["count"] = counter + 1;
+			}
+		});
+			console.log('New Count is: ' + counter);
+			console.log(msg.author.username + "#" + msg.author.discriminator);
+		} else {
+			//if (!messups[msg.author.id]) { messups[msg.author.id] = '{"messups": 0,}' }
+			//messups[msg.author.id].messups++;
+			//if (messups[msg.author.id].messups >= 3) {
+				//msg.author.username.addRole(cantcount).catch(console.error); // add the role to the user
+			//}
+			msg.delete();
+		}
+		//fs.writeFile("./messups.json", JSON.stringify(messups), (err) => {
+		//	if (err) console.error(err);
+		//}
+	}
+});
+
+bot.connect();
+
+/*
+const Eris = require("eris");
+//const fs = require("fs");
+const config = require("../config/config.json");
+var bot = new Eris(config.token);
+
+//let messups = JSON.parse(fs.readFileSync("./messups.json", "utf8"));
+
+var counter = 0;
+
+bot.on("ready", () => {
+	console.log("Ready to moderate counting!");
+	console.log("Logged in as " + bot.user.username + "#" + bot.user.discriminator + ".");
+	
+	bot.editStatus("online", { name: "1 2 3...", type: 0 });
+});
+
+bot.on("messageCreate", (msg) => {
+	//let cantcount = msg.guild.roles.get("384107437998735363");
+	if (msg.author.bot) return;
+	//if (!messups[msg.author.id]) messups[msg.author.id] = {
+	//	messups: 0;
+	//}
+	if (msg.channel.id == (config.channel)) {
+		if (msg.content == counter) {
+			counter++;
+			console.log('New Count is: ' + counter);
+			console.log(msg.author.username + "#" + msg.author.discriminator);
+		} else {
+		//	messups[msg.author.id].messups++;
+			//if (messups[msg.author.id].messups >= 3) {
+				//msg.author.username.addRole(cantcount).catch(console.error); // add the role to the user
+			//}
+			msg.delete();
+		}
+		//fs.writeFile("./messups.json", JSON.stringify(messups), (err) => {
+		//	if (err) console.error(err);
+		//}
+	}
+});
+
+bot.connect();
+*/
+
+
+/* OLD
+const Eris = require("eris");
 const config = require("../config/config.json");
 
 var bot = new Eris(config.token);
@@ -20,7 +114,7 @@ bot.on("messageCreate", (msg) => {
         bot.createMessage(msg.channel.id, 'Pinging...').then(sent => {
 			    sent.edit(`**Pong!** \nMy latency is \`${sent.timestamp - msg.timestamp}ms\`.`);
 		    })
-    }; // End of ping commands
+    };
 
     // main module ---
     if(msg.channel.id != (config.channel)) {
@@ -29,13 +123,13 @@ bot.on("messageCreate", (msg) => {
     } else {
       var newCount = (counter + 1);
 
-      if (msg.content < newCount || msg.content > newCount || msg.content < 0 || isNaN(msg.content)) {
+      if (msg.content != newCount) {
         msg.delete("Auto-removed wrong or non-related message in counting channel.");
 
-      }/* else if (msg.author.id === lastCountUser) {
+      } else if (msg.author.id === lastCountUser) {
         msg.delete("Auto-removed user's double message in counting channel.");
 
-      } */else {
+      } else {
         counter = newCount;
         lastCountUser = msg.author.id;
       }
@@ -44,3 +138,4 @@ bot.on("messageCreate", (msg) => {
 });
 
 bot.connect();
+*/
