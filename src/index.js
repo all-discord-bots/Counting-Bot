44368042a24ac6df405b582f7a047359b0dc6d30
BannerBomb <BannerBomb55@gmail.com>
2017-11-25 19:31:54 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,98 @@
 const Eris = require("eris");
+const fs = require("fs");
+const config = require("../config/config.json");
+var bot = new Eris(config.token);
+
+//let messups = JSON.parse(fs.readFileSync("./messups.json", "utf8"));
+let count = JSON.parse(fs.readFileSync("./count.json", "utf8"));
+
+var counter = count["count"];
+
+bot.on("ready", () => {
+	console.log("Ready to moderate counting!");
+	console.log("Logged in as " + bot.user.username + "#" + bot.user.discriminator + ".");
+	
+	bot.editStatus("online", { name: "1 2 3...", type: 0 });
+});
+
+bot.on("messageCreate", (msg) => {
+	//let cantcount = msg.guild.roles.get("384107437998735363");
+	if (msg.author.bot) return;
+	if (msg.channel.id == (config.channel)) {
+		if (msg.content == counter) {
+			counter++;
+			fs.writeFile("./count.json", JSON.stringify(count), (err) => {
+			if (err) {
+				console.error(err);
+			} else {
+				count["count"] = counter + 1;
+			}
+		});
+			console.log('New Count is: ' + counter);
+			console.log(msg.author.username + "#" + msg.author.discriminator);
+		} else {
+			//if (!messups[msg.author.id]) { messups[msg.author.id] = '{"messups": 0,}' }
+			//messups[msg.author.id].messups++;
+			//if (messups[msg.author.id].messups >= 3) {
+				//msg.author.username.addRole(cantcount).catch(console.error); // add the role to the user
+			//}
+			msg.delete();
+		}
+		//fs.writeFile("./messups.json", JSON.stringify(messups), (err) => {
+		//	if (err) console.error(err);
+		//}
+	}
+});
+
+bot.connect();
+
+/*
+const Eris = require("eris");
+//const fs = require("fs");
+const config = require("../config/config.json");
+var bot = new Eris(config.token);
+
+//let messups = JSON.parse(fs.readFileSync("./messups.json", "utf8"));
+
+var counter = 0;
+
+bot.on("ready", () => {
+	console.log("Ready to moderate counting!");
+	console.log("Logged in as " + bot.user.username + "#" + bot.user.discriminator + ".");
+	
+	bot.editStatus("online", { name: "1 2 3...", type: 0 });
+});
+
+bot.on("messageCreate", (msg) => {
+	//let cantcount = msg.guild.roles.get("384107437998735363");
+	if (msg.author.bot) return;
+	//if (!messups[msg.author.id]) messups[msg.author.id] = {
+	//	messups: 0;
+	//}
+	if (msg.channel.id == (config.channel)) {
+		if (msg.content == counter) {
+			counter++;
+			console.log('New Count is: ' + counter);
+			console.log(msg.author.username + "#" + msg.author.discriminator);
+		} else {
+		//	messups[msg.author.id].messups++;
+			//if (messups[msg.author.id].messups >= 3) {
+				//msg.author.username.addRole(cantcount).catch(console.error); // add the role to the user
+			//}
+			msg.delete();
+		}
+		//fs.writeFile("./messups.json", JSON.stringify(messups), (err) => {
+		//	if (err) console.error(err);
+		//}
+	}
+});
+
+bot.connect();
+*/
+
+
+/* OLD
+const Eris = require("eris");
 const config = require("../config/config.json");
 
 var bot = new Eris(config.token);
@@ -44,3 +138,4 @@ bot.on("messageCreate", (msg) => {
 });
 
 bot.connect();
+*/
